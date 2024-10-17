0cd6ec5ae9be5ac06d46ab8eee16e21dcae0c961
m410n3s3 <gustavolaurentino2009@gmail.com>
2024-10-17 02:30:20 -0300

    omgcode.py

--- o.py
+++ o.py
@@ -1,5 +1,34 @@
 
-a=float(input())
-b=float(input())
-x=a+b
-print("X = {:.0f}".format(x))
+# a=float(input())
+# b=float(input())
+# x=a+b
+# print("X = {:.0f}".format(x))
+
+N = int(input("Escreva o número de repetições: "))
+contador = 0
+
+def funcao():
+    S = int(input("Escreva o número real: "))
+    QT = int(input("Escreva a quantidade de alunos: "))
+    while True:
+        palpites = list(map(int, input(f"Digite {QT} palpites, separados por espaço: ").split()))
+        if len(palpites) == QT:
+            break
+        else:
+            print(f"Repete, insira exatamente {QT} palpites.")
+
+    melhor_palpite = None
+    menor_diferenca = float('inf')
+    
+    for palpite in palpites:
+        diferenca = abs(S - palpite)
+        if diferenca < menor_diferenca:
+            menor_diferenca = diferenca
+            melhor_palpite = palpite
+            
+    print(f"O melhor palpite é: {melhor_palpite}")
+
+if __name__ == "__main__":
+    while contador < N:
+        funcao()
+        contador += 1
